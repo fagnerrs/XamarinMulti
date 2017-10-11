893c7957d45eb3deea42763954c81df6e1dce368
Fagner Oliveira <fagneroliveira@Fagners-MacBook-Pro.local>
2017-10-10 19:05:25 -0500

    updating obj

---
+++ Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Fwit.Genone.Droid.MainApplication, Fwit.Genone.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5e43a30944322235846e9e508c18104f3.MainApplication.class, md5e43a30944322235846e9e508c18104f3.MainApplication.__md_methods);
+		
+	}
+}
